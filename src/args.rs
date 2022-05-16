479cb5cc37baa5b2f89c06087d8b691e1179989b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-16 20:28:37 +0300

    add new args

--- src/args.rs
+++ src/args.rs
@@ -6,12 +6,20 @@ use clap::Parser;
 #[clap(propagate_version = true)]
 pub struct ranArgs {
     #[clap(short = 'm', long, default_value_t = 1)]
-    pub min: u32,
+    pub min: i32,
 
     #[clap(short = 'M', long, default_value_t = 100)]
-    pub max: u32,
+    pub max: i32,
 
-    // Number of generated numbers
+    /// Number of generated numbers
     #[clap(short = 'c', long, default_value_t = 1)]
     pub count: u32,
+
+    /// Inline print all generated numbers
+    #[clap(short = 'i', long)]
+    pub inline: bool,
+
+    /// Change inline print delimiter
+    #[clap(short = 'd', long, default_value = " ")]
+    pub delimiter: String,
 }
