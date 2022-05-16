4be949302372db87fed4414779fa6d38e4549cf2
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-16 21:06:41 +0300

    main

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,32 @@ mod args;
 
 use args::ranArgs;
 use clap::Parser;
+use rand::Rng;
 
 fn main() {
     let cli = ranArgs::parse();
 
-    println!("min: {}\nmax: {}", cli.min, cli.max);
+    if !cli.inline {
+        for _ in 0..cli.count {
+            let random_number: i32 = rand::thread_rng().gen_range(cli.min..cli.max);
+            println!("{}", random_number);
+        }
+    }
+
+    if cli.inline && cli.count > 1 {
+        for c in 1..cli.count + 1 {
+            let random_number: i32 = rand::thread_rng().gen_range(cli.min..cli.max);
+            if cli.delimiter_before {
+                print!("{}", cli.delimiter);
+            }
+
+            print!("{}", random_number);
+
+            if cli.delimiter_after || !cli.delimiter_after && !cli.delimiter_before {
+                if c != cli.count {
+                    print!("{}", cli.delimiter);
+                }
+            }
+        }
+    }
 }
